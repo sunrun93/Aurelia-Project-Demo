0b941062b5ea174ae368768d9a0f7845ca3af7f5
sunrun93 <wangjian9302@163.com>
2017-10-18 16:55:25 +0800

    update aurelia rooter

--- src/header/header.ts
+++ src/header/header.ts
@@ -2,19 +2,12 @@ import {autoinject} from 'aurelia-framework';
 import { bindable, customElement } from 'aurelia-templating';
 import {AppRouter, RouterConfiguration} from 'aurelia-router';
 
-//@customElement("header-part")
+@customElement("header-part")
 @autoinject
 export class Header{
-    private router:AppRouter;
-
     constructor(){
-
-        let b = 1;
     }
 
-    
-
     attached(){
-        let a = 2;
     }
 }
