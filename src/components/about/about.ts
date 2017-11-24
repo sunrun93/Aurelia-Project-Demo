be75fc1060006ce7f21c6fb7ea69e910edfb0fc1
sunrun93 <wangjian9302@163.com>
2017-11-24 15:52:59 +0800

    add right click context menu

--- src/components/about/about.ts
+++ src/components/about/about.ts
@@ -1,6 +1,8 @@
 export class About {
     private nodeItems = [];
     private rootNodes = [];
+    private color = "red";
+
     constructor(){
 
     }
@@ -11,13 +13,17 @@ export class About {
             { id: '2', name: 'test2', isGroup:false},
             { id: '3', name: 'test3', isGroup:true },
             { id: '4', name: 'test4', isGroup:false },
-            { id: '5', name: 'test1-1', parentId: '1',equation:"AAAAAAAAA" },
+            { id: '5', name: 'test1-1', parentId: '1',equation:"AAAAAAAAA"},
             { id: '6', name: 'test1-2', parentId: '1',equation:"BBBBBBBBB"},
             { id: '7', name: 'test3-1', parentId: '3',equation:"CCCCCCCCC"}
           ];
         this.renderBasicNode(); 
     }
 
+    private callBackFun(){
+      console.log("AAA");
+
+    }
     
     private renderBasicNode(){
         this.nodeItems.forEach((node)=>{
@@ -33,5 +39,13 @@ export class About {
       private getChildren(node){
         let children = this.nodeItems.filter((n)=>{return n.parentId === node.id});
         node.children = children;
+      }
+
+      private changeColor() {
+        if (this.color == "red") {
+          this.color = "yellow";
+        } else {
+          this.color = "red";
+        }
       }
 }
--- src/components/about/tree-node.ts
+++ src/components/about/tree-node.ts
@@ -1,7 +1,57 @@
-import { autoinject } from 'aurelia-framework';
+import { autoinject,observable  } from 'aurelia-framework';
 import { bindable } from 'aurelia-templating';
 
 @autoinject
 export class TreeNode {
     @bindable treeNode;
+    @observable color = 'blue';
+
+    private showMenu = false;
+    private menu:Element;
+    
+    attached(){
+        this.addEventListener();
+    }
+
+    colorChanged(oldValue,newValue){
+        console.log(newValue);
+    }
+
+    private addEventListener() {
+        document.body.addEventListener("mousedown", (e: MouseEvent) => {
+            this.showMenu = false;
+        });
+    }
+
+    private showContextMenu(e:Event){
+        e.stopPropagation();
+        e.preventDefault();
+        console.log("ssssss");
+        this.showMenu = true;
+        setTimeout(()=>{
+            this.menu["style"].left = e["clientX"]+"px";
+            this.menu["style"].top = e["clientY"]+"px";
+        },0);
+    }
+
+    private showChildNode:boolean=false;
+
+    private toggleChildNode(){
+        this.showChildNode = !this.showChildNode;
+    }
+
+    private itemClick(){
+        console.log("AAAAAAAA");
+        this.showMenu = false;
+    }
+    
+    private showName(node){
+        console.log(node.id);
+
+    }
+
+    private colorChange(newVlaue, oldValue){
+
+    }
+  
 }
